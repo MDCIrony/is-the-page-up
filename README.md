e3edfa088ff39dca514b4000d5d046df10b0c630
MDCIrony <miguel.acastillodiaz@outlook.es>
2023-03-23 01:09:15 -0500

    Some progress for today

---
+++ README.md
@@ -0,0 +1,4 @@
+# Is this Page UP?
+
+This is a simple script to check if a website is up or down.
+Adittionally, it can send an message on telegram to you if the website is up.
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from dotenv import dotenv_values
 import requests
 import time
+import telegram
+import asyncio
+
+
+# Type hinting
 from typing import Dict
 
 # Dotenv values
 config: Dict[str, str | None] = dotenv_values(".env")
+
+
+async def main():
+    bot = telegram.Bot(token=config["API_BOT_TELEGRAM_TOKEN"])
+    async with bot:
+        print(await bot.get_me())
+
+
+async def updates():
+    bot = telegram.Bot(token=config["API_BOT_TELEGRAM_TOKEN"])
+    async with bot:
+        print((await bot.get_updates())[0])
 
 
 def check_website_status():
@@ -16,13 +33,18 @@ def check_website_status():
         return False
 
 
-if __name__ == "__main__":
-    while True:
-        if check_website_status():
-            print(
-                "La página está en línea; corroborando estado nuevamente en 1 minuto."
-            )
-        else:
-            print("La página no está en línea. Reintentando en 1 minuto.")
+def send_message():
+    pass
 
-        time.sleep(60)
+
+if __name__ == "__main__":
+    # while True:
+    #     if check_website_status():
+    #         print(
+    #             "La página está en línea; corroborando estado nuevamente en 1 minuto."
+    #         )
+    #     else:
+    #         print("La página no está en línea. Reintentando en 1 minuto.")
+
+    #     time.sleep(60)
+    asyncio.run(updates())
